0992793df1458b80bebaa5933d33cb4cb93e2c33
Vishnu Rathore <vr8043836@gamil.com>
2022-08-15 03:16:53 +0530

    graph-update

---
+++ myapp/queries/index.js
@@ -0,0 +1,14 @@
+export function FETCH_CREATED_GAME() {
+    return `query {
+          games(orderBy:id, orderDirection:desc, first: 1) {
+              id
+              maxPlayers
+              entryFee
+              winner
+              players
+          }
+      }`;
+  }
+
+  //we created a GraphQL query where we said we want a game object where data is ordered by Id(which is the gameId) in 
+  //decending order and we want the first game from this ordered data
